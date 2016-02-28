9bddff8567d9da5b04ea2b718bf435ae2b70cc93
liwentao0705 <liwentao0705@outlook.com>
2016-02-28 20:18:22 +0800

    build: improve O3

--- core/optimizations/O3.mk
+++ core/optimizations/O3.mk
@@ -20,19 +20,6 @@ HE_CFLAGS := \
 
 HE_CPPFLAGS := \
 	-fvisibility-inlines-hidden
-
-HE_CLANG_CFLAGS := \
-	-O3 \
-	-Qunused-arguments \
-	-Wno-unknown-warning-option
-
-HE_CLANG_CPPFLAGS := \
-	-O3 \
-	-Qunused-arguments \
-	-Wno-unknown-warning-option \
-	-D__compiler_offsetof=__builtin_offsetof
-
-HE_CLANG_LDFLAGS := -Wl,--sort-common
 else
 HE_CFLAGS_ARM := \
 	-O2 \
@@ -56,6 +43,20 @@ HE_CFLAGS := \
 HE_CPPFLAGS := \
 	-fvisibility-inlines-hidden
 
+ifeq ($(CLANG_O3),true)
+HE_CLANG_CFLAGS := \
+	-O3 \
+	-Qunused-arguments \
+	-Wno-unknown-warning-option
+
+HE_CLANG_CPPFLAGS := \
+	-O3 \
+	-Qunused-arguments \
+	-Wno-unknown-warning-option \
+	-D__compiler_offsetof=__builtin_offsetof
+
+HE_CLANG_LDFLAGS := -Wl,--sort-common
+else
 HE_CLANG_CFLAGS :=
 HE_CLANG_CPPFLAGS :=
 HE_CLANG_LDFLAGS :=
